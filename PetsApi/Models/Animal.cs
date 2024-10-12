c57b6b3a4511d9f4f1947b1dd011bdc9a3687664
NathanGab <151534404+NathanGab@users.noreply.github.com>
2024-10-11 21:26:30 -0300

    Add files via upload

---
+++ PetsApi/Models/Animal.cs
@@ -0,0 +1,10 @@
+public class Animal
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Cor { get; set; }
+    public string Tipo { get; set; }
+    public double PesoKg { get; set; }
+    public bool Vacinado { get; set; }
+}
