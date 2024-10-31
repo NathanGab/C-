d5b1f260fadc3a8ae46e60ecfcc3c9716f990085
NathanGab <151534404+NathanGab@users.noreply.github.com>
2024-10-31 20:39:14 -0300

    Add files via upload

---
+++ BibliotecaApi/Data/BibliotecaContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using BibliotecaApi.Models;
+
+namespace BibliotecaApi.Data
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Biblioteca> Bibliotecas { get; set; }
+    }
+}
